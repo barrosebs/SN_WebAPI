cd70873b371342aa58071ff2e9d63dada1b01a5b
Eduardo Barros <barrosebs@gmail.com>
2020-01-22 23:04:34 -0300

    Primeiros passos do projeto

---
+++ SN.WebAPI/Model/Atleta.cs
@@ -0,0 +1,30 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SN.WebAPI.Model
+{
+    [Table("Atletas")]
+    public class Atleta
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name="Informe o nome completo do atleta")]
+        public string Nome { get; set; }
+        [Required]
+        [Display(Name="Informe o apelido do atleta")]
+        public string Apelido { get; set; }
+        [Required]
+        [Display(Name="Data de Nascimento do Atleta")]
+        [DisplayFormat(DataFormatString="dd/MM/yyyy")]
+        public DateTime? Nascimento { get; set; }
+        [Required]
+        [Display(Name = "Informe o Telefone do Atleta")]
+        [DisplayFormat(DataFormatString="(99) 9 9999-9999")]
+        public string Telefone { get; set; }
+        public DateTime DataInscricao { get; set; }
+        public int FuncaoAtleta { get; set; }
+        public bool Comissao { get; set; }
+    }
+}
